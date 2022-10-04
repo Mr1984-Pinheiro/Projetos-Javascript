06baff5fa0c0442b35d5363bd1dffaca911472b3
Eduardo <cadutapinheiro@gmail.com>
2022-10-04 04:26:24 -0300

    Validação em todos os campos

--- formulario 2/index.js
+++ formulario 2/index.js
@@ -22,6 +22,42 @@ function checkInputs() {
         errorValidation(username, 'Preencha esse campo')        
     } else {
         //adicionar a classe de sucesso
+        successValidation(username)
+    }
+
+    if (emailValue === '')  {        
+        // mostrar o erro
+        // adicionar a classe error
+        errorValidation(email, 'Preencha esse campo')        
+    } else {
+        //adicionar a classe de sucesso
+        successValidation(email)
+    }
+
+    if (passwordValue === '')  {        
+        // mostrar o erro
+        // adicionar a classe error
+        errorValidation(password, 'Preencha esse campo')  
+              
+    } else if (passwordValue.length < 8) {
+        errorValidation(password, 'A senha deve conter mais de 8 caracteres')  
+    }
+    
+    else {
+        //adicionar a classe de sucesso
+        successValidation(password)
+    }
+
+    if (password2Value === '')  {        
+        // mostrar o erro
+        // adicionar a classe error
+        errorValidation(password2, 'Preencha esse campo')        
+    } else if(passwordValue !== password2Value) {
+        errorValidation(password2, 'As senhas precisam ser iguais')
+    } 
+    else {
+        //adicionar a classe de sucesso
+        successValidation(password2)
     }
 }
 
@@ -32,4 +68,10 @@ function errorValidation(input, message) {
     small.innerText = message
 
     formControl.className = 'form-control error'
+}
+
+function successValidation (input) {
+    const formControl = input.parentElement;   
+
+    formControl.className = 'form-control success'
 }
