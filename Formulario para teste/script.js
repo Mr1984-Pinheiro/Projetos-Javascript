6d363c24f3c09064d200faf4902e26d6319cfd68
Eduardo <cadutapinheiro@gmail.com>
2022-10-06 04:08:48 -0300

    Exibindo erro

--- Formulario para teste/script.js
+++ Formulario para teste/script.js
@@ -5,13 +5,15 @@ let Gamervalidation = {
 
     let inputs = form.querySelectorAll('input')
 
+    Gamervalidation.clearErrors();
+
     for (let i = 0; i < inputs.length; i++) {
       let input = inputs[i];
       let check = Gamervalidation.checkInput(input)
       if(check !== true) {
         send = false 
-        console.log(check)// Exibir o error
-        alert(check);
+       // Exibir o error
+        Gamervalidation.showError(input, check);
       }
       
     }
@@ -43,6 +45,29 @@ let Gamervalidation = {
       }
     }
     return true;
+  },
+
+  showError: (input, error) => {
+      input.style.borderColor = '#FF0000';
+
+      let errorElement = document.createElement('div');
+      errorElement.classList.add('error');
+      errorElement.innerHTML = error;
+
+      input.parentElement.insertBefore(errorElement, input.ElementSibling);
+  },
+
+  clearErrors: ()=> {
+
+    let noStyle = form.querySelectorAll('input');
+    for(let i=0; i<noStyle.length; i++){
+      noStyle[i].style = '';
+    };
+
+    let errorElements = document.querySelectorAll('.error');
+    for(let i=0; i<errorElements.length; i++) {
+      errorElements[i].remove();
+    }
   }
 };
 
