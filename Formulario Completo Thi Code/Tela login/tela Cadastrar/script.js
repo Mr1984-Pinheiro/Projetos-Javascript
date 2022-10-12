0de272438693d17c8a08034becdb86ec6b619511
CARLOS EDUARDO TAVARES PINHEIRO <73333067+Mr1984-Pinheiro@users.noreply.github.com>
2022-10-12 04:34:46 -0300

    instanciando função entrar

--- Formulario Completo Thi Code/Tela login/tela Cadastrar/script.js
+++ Formulario Completo Thi Code/Tela login/tela Cadastrar/script.js
@@ -9,3 +9,26 @@ showPassword.addEventListener('click', () => {
         inputPassword.setAttribute('type', 'password' )
     }
 })
+
+function entrar() {
+   let usuario = document.querySelector('#usuario')
+   let userLabel = document.querySelector('#userLabel')
+
+   let senha = document.querySelector('#senha')
+   let senhaLabel = document.querySelector('#senhaLabel')
+
+   let msgError = document.querySelector('#msgError')
+   let listaUser = []
+
+   let userValid = {
+    nome: '',
+    user: '',
+    senha: ''
+   }
+
+   listaUser = JSON.parse(localStorage.getItem('listaUser'))
+   
+   listaUser.forEach((item) => {
+    
+   })
+}
